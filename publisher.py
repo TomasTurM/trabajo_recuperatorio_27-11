9b5162106afb93699a0330f78ad1e4af1e832fd2
TomasM tur <tomasm@tomassystem3.0>
2019-11-27 10:55:07 -0300

    avance: introduccion del canal

--- publisher.py
+++ publisher.py
@@ -4,14 +4,16 @@ import redis
 r = redis.Redis(host='localhost', port=6379)
 
 def RedisPublisher():
-	print('Canal alpha')
+	channel = input('Introduzca canal de Redis: ')
+	channel = str(channel)
+	print('Canal ' + channel)
 	user_msg = input('Introduzca mensaje: ')
 	user_msg = str(user_msg)
 	frequency = input('Introduzca frecuencia en segundos: ')
 	frequency = float(frequency)
 
 	while True:
-		r.publish('alpha', user_msg)
+		r.publish(channel, user_msg)
 		time.sleep(frequency)
 
 RedisPublisher()
--- subscriber.py
+++ subscriber.py
@@ -2,9 +2,12 @@ import redis
 r = redis.Redis(host='localhost', port=6379)
 
 def RedisSuscriber():
+	channel = input('Introduzca canal de Redis a escuchar: ')
+	channel = str(channel)
+	print('Canal ' + channel)
 	sub = r.pubsub()
-	sub.subscribe('alpha')
-	print('Conectado al canal alpha')
+	sub.subscribe(channel)
+	print('Conectado al canal ' + channel)
 
 	while True:
 		msg = sub.get_message()
